17d0d7d3372f5995f7bf8bb80a6449f327bd8c1e
wolandark <contact-woland@proton.me>
2023-04-23 23:11:25 +0330

    Improve Install Script

--- Wim_Arch_Install.sh
+++ Wim_Arch_Install.sh
@@ -8,23 +8,63 @@
 # https://github.com/wolandark
 # https://github.com/wolandark/wim
 
-sudo pacman -S nodejs npm figlet fzf curl git vim
+# Define required files and directories
+WIM_VIMRC="$HOME/wim/wim-3.1.vimrc"
+ROMAN_FLF="/usr/share/figlet/fonts/roman.flf"
+BOXES_DIR="$HOME/boxes"
 
-git clone https://github.com/wolandark/wim.git
-git clone https://aur.archlinux.org/boxes.git
+# Install required packages
+sudo pacman -S nodejs npm figlet fzf curl git vim || {
+    echo "Error: Failed to install required packages"
+    exit 1
+}
 
-cd ~/boxes
-makepkg -sri
-cd
+# Clone repositories
+git clone https://github.com/wolandark/wim.git || {
+    echo "Error: Failed to clone wim repository"
+    exit 1
+}
 
-mv $HOME/wim/wim-3.1.vimrc $HOME/.vimrc
-sudo mv $HOME/wim/roman.flf /usr/share/figlet/fonts/
+git clone https://aur.archlinux.org/boxes.git || {
+    echo "Error: Failed to clone boxes repository"
+    exit 1
+}
 
+# Check if required files and directories exist
+if [[ ! -f "$WIM_VIMRC" ]]; then
+    echo "Error: $WIM_VIMRC does not exist"
+    exit 1
+fi
+
+if [[ ! -f "$ROMAN_FLF" ]]; then
+    echo "Error: $ROMAN_FLF does not exist"
+    exit 1
+fi
+
+if [[ ! -d "$BOXES_DIR" ]]; then
+    echo "Error: $BOXES_DIR does not exist"
+    exit 1
+fi
+
+# Build and install boxes
+cd "$BOXES_DIR"
+makepkg -sri || {
+    echo "Error: Failed to build and install boxes"
+    exit 1
+}
+cd -
+
+# Move files to their respective locations
+mv "$WIM_VIMRC" "$HOME/.vimrc" || {
+    echo "Error: Failed to move wim-3.1.vimrc to $HOME/.vimrc"
+    exit 1
+}
+
+sudo mv "$HOME/wim/roman.flf" "$ROMAN_FLF" || {
+    echo "Error: Failed to move roman.flf to $ROMAN_FLF"
+    exit 1
+}
+
+# All done!
 echo "All Done! You can now start using Wim"
 exit 0
-#Vim Plug installation command for Unix-Gnu/Linux 
-#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
-

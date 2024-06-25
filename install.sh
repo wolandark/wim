a2dc6e2112bd14980db5dfcac585fdde856cb5ca
wolandark <isitjuicyisitfresh@tutanota.com>
2024-06-26 03:06:31 +0330

    Update script main func

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ _ARCHIVE_EXTRACTED_NAME="uctags-2024.06.20-linux-x86_64.tar.gz"
 _COC_JSON="https://raw.githubusercontent.com/wolandark/wim/master/config/coc-settings.json"
 _CHEAT_FILE="https://raw.githubusercontent.com/wolandark/wim/master/config/cheat40.txt"
 _VIMRC_FILE="$_VIM_PATH/vimrc"
+_VIMRC_FILE_REMOTE="https://raw.githubusercontent.com/wolandark/wim/master/wim-vim"
 _VIMWIKI_PATH="$_VIM_PATH/vimwiki"
 _VIMWIKI_INDEX="$_VIMWIKI_PATH/index.wiki"
 _VIMWIKI_WIM="$_VIMWIKI_PATH/Wim.wiki"
@@ -39,6 +40,7 @@ getFiles()
 {
 	curl -fLo "$_VIM_PATH"/cheat40.txt "$_CHEAT_FILE" 
 	curl -fLo  "$_VIM_PATH"/coc-settings.json "$_COC_JSON"
+	curl -fLo "$_VIMRC_FILE" "_$_VIMRC_FILE_REMOTE"
 }
 
 setupVimWiki()
@@ -48,4 +50,12 @@ setupVimWiki()
 	echo "[[Wim]]" > "$_VIMWIKI_PATH"/"$_VIMWIKI_INDEX"
 }
 
-vim 
+main()
+{
+	backup 
+	downloadCtags 
+	getFiles
+	setupVimWiki
+}
+
+main
